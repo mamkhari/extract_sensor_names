e01a6223257199ae204215857815ed5e00af1dfa
Lebogang <llekganyane@ska.ac.za>
2020-02-24 16:30:04 +0200

    labelling variables

--- extract_unique_sensors.py
+++ extract_unique_sensors.py
@@ -1,3 +1,4 @@
+
 from urllib.request import urlopen
 
 sensors_data = urlopen('http://monctl.mkat-rts.karoo.kat.ac.za/kat/doc/manuals/sensors/SensorList.txt')
@@ -7,17 +8,17 @@ candidate_lines = []
 for line in lines:
     line = line.decode()
     if line.startswith('kat.sensors'):
-        for sensor in line.split():
-            candidate_lines.append(sensor)
+        for candidate_line in line.split():
+            candidate_lines.append(candidate_line)
 
 # extract sensor names
 
 sensor_names = []
-for sensor_name in candidate_lines:
-    # typical line : gvygvtvyvtvyvtv
+for sensor in candidate_lines:
+    # typical line :
     # kat.sensors.anc_api_version   kat.sensors.anc_ganglia_api_version   kat.sensors.anc_ganglia_kat_monctl_system_os_name
-    sensor_name = sensor_name.split('.')[-1].strip("\n")
-    sensor_names.append(sensor_name)
+    sensor = sensor.split('.')[-1].strip("\n")
+    sensor_names.append(sensor)
 print(sorted(sensor_names))
 
 
