8da1cf3086eb6ac0cada978e5fd9e0c40bee2f09
Lebogang <llekganyane@ska.ac.za>
2020-02-24 15:08:36 +0200

    Adding new file.

---
+++ extract_unique_sensors.py
@@ -0,0 +1,32 @@
+from urllib.request import urlopen
+
+sensors_data = urlopen('http://monctl.mkat-rts.karoo.kat.ac.za/kat/doc/manuals/sensors/SensorList.txt')
+lines = sensors_data.readlines()
+
+candidate_lines = []
+for line in lines:
+    line = line.decode()
+    if line.startswith('kat.sensors'):
+        for sensor in line.split():
+            candidate_lines.append(sensor)
+
+# extract sensor names
+
+sensor_names = []
+for sensor_name in candidate_lines:
+    # typical line : 
+    # kat.sensors.anc_api_version   kat.sensors.anc_ganglia_api_version   kat.sensors.anc_ganglia_kat_monctl_system_os_name
+    sensor_name = sensor_name.split('.')[-1].strip("\n")
+    sensor_names.append(sensor_name)
+print(sorted(sensor_names))
+
+
+
+
+
+
+
+
+
+
+
