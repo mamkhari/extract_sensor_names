d8d1b5bf08413608fa7a5f18bb5b406c4098d484
Lebogang Lekganyane <52751080+mamkhari@users.noreply.github.com>
2020-08-26 10:11:14 +0200

    Fixed the closing parenthesis
    Co-authored-by: Mpho Mphego <mmphego@ska.ac.za>

--- extract_unique_sensors_01.py
+++ extract_unique_sensors_01.py
@@ -17,8 +17,10 @@ def create_parser():
 
     parser = argparse.ArgumentParser(description='Extract unique sensors')
     parser.add_argument('--file_name', help='File to extract unique sensors from.')
-    parser.add_argument('--url', help='A reference to SensorList.txt that specifies its'
-                        'location on a computer network.')
+parser.add_argument(
+    '--url',
+    help='A reference to SensorList.txt that specifies its location on a computer network.'
+)
     parser.add_argument('--kat_sensor', required=True, help='Name of unique sensor')
     parser.add_argument('-v', metavar='verbosity', type=int, default=2, help='Logging'
                         'verbosity: 0 -critical, 1- error, 2 -warning, 3 -info, 4 -debug')
