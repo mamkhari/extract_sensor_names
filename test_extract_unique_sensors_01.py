7223e1a035da5917ced50c5234dca5c181e27516
mamkhari <lebogangmamkhari@gmail.com>
2020-09-11 10:01:31 +0200

    Added tests for filename and URL.

--- test_extract_unique_sensors_01.py
+++ test_extract_unique_sensors_01.py
@@ -1,10 +1,37 @@
 import unittest
+from mock import MagicMock
+from mock import patch
 from extract_unique_sensors_01 import extract_all_sensors
 from extract_unique_sensors_01 import splitting_extracted_sensors
 
 
 class TestExtractSensors(unittest.TestCase):
     """Test class for function test_read_sensors."""
+
+    def test_read_sensors(self):
+        with patch('requests.get') as mock_request:
+            url = "http://lebogang.com"
+            mock_request.return_value.status_code = 200
+            self.assertTrue(url)
+
+        with patch('requests.get') as mock_request:
+            non_existing_url = "http://google.com/fakeurl"
+            mock_request.return_value.status_code = 404
+            self.assertTrue(non_existing_url)
+
+        file_name = "katlego"
+        mock_path = MagicMock()
+        mock_path.Path(file_name).exists().return_value = True
+        self.assertTrue(file_name)
+        self.assertIsInstance(file_name, str)
+        self.assertIsInstance(url, str)
+
+        def Assertlogs(self):
+            with self.Assertlogs('url', 'file_name', level='DEBUG') as cm:
+                logging.debug("Unknown url: %r.", url)
+                logging.debug("Uknown file_name: %r.", file_name)
+                self.assertEqual(
+                    cm.output, ['BEBUG:url:Uknownn url', 'BEBUG:url:Uknownn url'])
 
     def test_extract_all_sensors(self):
         sensor_name = "kat"
